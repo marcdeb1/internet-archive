d53eb33bfdb479db6ed494e61bd1dae672c0ad0a
marcdeb1 <marcdebrouchev@laposte.net>
2017-06-21 14:48:12 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from InternetArchive import *
+from LBRYObject import *
+
+url = "http://localhost:5279/lbryapi"
+
+ia = InternetArchiveSearch()
+d = ia.get_identifiers()
+print("OK")
+transactions = list()
+for id in d:
+    identifier = id['identifier']
+    print(identifier)
+    document = ia.get_metadata(identifier)
+    url = ia.get_file_url(document)
+    f = ia.download_file(url)
+    lbry_object = LBRYObject(f, document)
+    # transaction = lbry_object.publish()
+    # transactions.append(transaction)
