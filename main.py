19b3d55bf14ba32edd9b31c1cb6ecb63515a1f42
marcdeb1 <marcdebrouchev@laposte.net>
2018-10-16 13:09:53 +0200

    Added command line tool

--- main.py
+++
@@ -1,5 +0,0 @@
-from InternetArchive import *
-
-collection_name = "coverartarchive"
-ia = InternetArchive(collection_name)
-ia.upload_collection()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lbry_internet_archive',
+      version='0.1',
+      description='A tool to import Internet Archive collections to LBRY',
+      url='https://github.com/marcdeb1/internet-archive',
+      author='marcdeb',
+      author_email='marcdebrouchev@laposte.net',
+      license='MIT',
+      packages=['lbry_internet_archive'],
+      zip_safe=False)
