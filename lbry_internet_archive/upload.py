986b30545cdb2f0cf7a0569876598754b9beff15
marcdeb1 <marcdebrouchev@laposte.net>
2018-10-16 14:29:52 +0200

    Classes and files

--- lbry_internet_archive/upload.py
+++ lbry_internet_archive/upload.py
@@ -4,8 +4,8 @@ import click
 @click.command()
 @click.option('--collection', default='', help='Internet Archive collection name to upload.')
 @click.option('--config', default='default', help='Configuration file.')
-def upload(collection_name, config):
-	ia = InternetArchive(collection_name=collection_name, config_name=config)
+def upload(collection, config):
+	ia = InternetArchive(collection_name=collection, config_name=config)
 	ia.upload_collection()
 
 if __name__ == '__main__':
