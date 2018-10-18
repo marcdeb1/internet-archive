1c121e679b67920aa2502bb14cafcfbfac880d47
marcdeb1 <marcdebrouchev@laposte.net>
2018-10-18 06:05:00 +0200

    Requirements and setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='lbry_internet_archive',
-      version='0.1',
+      version='0.2',
       description='A tool to import Internet Archive collections to LBRY',
       url='https://github.com/marcdeb1/internet-archive',
       author='marcdeb',
       author_email='marcdebrouchev@laposte.net',
       license='MIT',
       packages=['lbry_internet_archive'],
+      insall_requires=['lbry-uploader', 'internetarchive', 'click', 'slugify'],
       zip_safe=False)
